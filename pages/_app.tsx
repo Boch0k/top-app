7ef53961b62bb032182f6c9c5bb4a8a88d7615a4
Boch0k <73186867+Boch0k@users.noreply.github.com>
2022-05-17 16:09:08 +0300

    first commit

---
+++ pages/_app.tsx
@@ -0,0 +1,14 @@
+import '../styles/globals.css';
+import Head from 'next/head';
+import React from 'react';
+import { AppProps } from 'next/dist/shared/lib/router/router';
+function MyApp({ Component, pageProps }: AppProps): JSX.Element {
+	return <>
+		<Head>
+			<title>MyTop - наш лучший топ</title>
+			<link rel="icon" href="/favicon.ico" />
+		</Head>
+		<Component {...pageProps} />
+	</>;
+}
+export default MyApp;
